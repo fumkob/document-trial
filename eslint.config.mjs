98e78362f875b7a88e793637601547c96015b7f0
Fumiaki Kobayashi <fumiaki-kobayashi@mail.nissan.co.jp>
2025-04-10 20:24:04 +0900

    Introduce formatter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,31 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["node_modules/**", ".next/**", "out/**"],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  },
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:prettier/recommended"
+  ),
+  {
+    rules: {
+      "prettier/prettier": "error",
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
